849963bb67dd828d39890972975f400b1bb9dc86
Bruce C.W.L <bruceadmin@Bruces-MBP.fritz.box>
2018-01-07 12:19:09 +0100

    Add excercise-2: hanoi tower

---
+++ towerOfHanoi.hs
@@ -0,0 +1,9 @@
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi layerCount start temp dest
+    | layerCount <= 0 = []
+    | layerCount == 1 = [(start, dest)]
+    | layerCount == 2 = [(start, temp), (start, dest), (temp, dest)]
+    | layerCount > 2 = hanoi (layerCount - 1) start dest temp ++ [(start, dest)] ++ hanoi (layerCount - 1) temp start dest
