5e9ad724da8512a31719bb28e64f431da9d77a57
Bruce C.W.L <bruceadmin@Bruces-MBP.fritz.box>
2018-01-07 12:23:42 +0100

    Remove redundant hs files

--- toDigitsRev.hs
+++
@@ -1,8 +0,0 @@
---Break an Integer into [Integer]
-divideByTen :: Integer -> Integer
-divideByTen = floor . realToFrac . (/10) . fromIntegral
-
-toDigits :: Integer -> [Integer]
-toDigits x  
-    | x < 10 = [x] 
-    | otherwise = [mod x 10] ++ (toDigits $ divideByTen x)
